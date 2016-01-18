d7bfc19ae5f9ebb7dea684515327ad25090092ef
k2229293 <453992341@qq.com>
2016-01-19 01:00:14 +0800

    x

--- mysite/views.py
+++ mysite/views.py
@@ -6,6 +6,8 @@ import requests
 import datetime
 import os.path
 
+base_dir=os.path.dirname(__file__)
+
 def index(request):
 	return render_to_response('index.html')
 	#return render_to_response('js/functions.js')
@@ -13,18 +15,18 @@ def statics(requests,x):
 	return render_to_response(x)
 
 def default(request):
-	f=open('css/default.css').read()
+	f=open(os.path.join(base_dir,'css/default.css')).read()
 	return HttpResponse(f)
 	#return render_to_response('css/default.css')
 def jquery(request):
-	f=open('js/jquery.js').read()
+	f=open(os.path.join(base_dir,'js/jquery.js')).read()
 	return HttpResponse(f)
 	#return render_to_response('js/jquery.js')
 def garden(request):
-	f=open('js/garden_dev.js').read()
+	f=open(os.path.join(base_dir,'js/garden_dev.js')).read()
 	return HttpResponse(f)
 	#return render_to_response('js/garden_dev.js')
 def functions(request):
-	f=open('js/functions_dev.js').read()
+	f=open(os.path.join(base_dir,'js/functions_dev.js')).read()
 	return HttpResponse(f)
 	#return render_to_response('js/functions_dev.js')
