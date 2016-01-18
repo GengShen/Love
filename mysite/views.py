766020b8bc3804144e3aa3ea7f88c7cad10bf339
k2229293 <453992341@qq.com>
2016-01-18 23:40:15 +0800

    set static 10

--- mysite/views.py
+++ mysite/views.py
@@ -5,10 +5,9 @@ from django.shortcuts import render_to_response
 import requests
 import datetime
 import os.path
-import os
 
 def index(request):
 	return render_to_response('index.html')
 
 def statics(requests,x):
-	return render_to_response(os.path.join(os.path.dirname(__file__), 'templates/'+x))
+	return render_to_response(x)
